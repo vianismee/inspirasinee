8d3a410d5c2bcc2f250940bc950a9b356b91fcba
vianismee <104370516+vianismee@users.noreply.github.com>
2025-08-24 11:49:47 +0700

    feat: Cart App V0.0.1

--- src/components/Cart/CartApp.tsx
+++ src/components/Cart/CartApp.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import { useCustomerStore } from "@/stores/customerStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export function CartApp() {
   const activeCustomer = useCustomerStore((state) => state.activeCustomer);
+  const router = useRouter();
+  useEffect(() => {
+    if (!activeCustomer) {
+      router.replace("/admin/input/");
+    }
+  }, [activeCustomer, router]);
   return <section>Nama: {activeCustomer?.customer}</section>;
 }
